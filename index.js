4d55af5a02ddfb52deb7ef5265f21440e9af2ee8
Daliso banda <112899523+Dalliso-banda@users.noreply.github.com>
2024-05-29 19:18:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let hide = document.getElementById('hide');
+let show = document.getElementById('show');
+
+
+hide.addEventListener('click',()=>{
+    show.classList.toggle('active')
+    
+    
+})
